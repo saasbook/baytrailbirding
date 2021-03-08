f179161e3ad3e44ee6c81e7ade7bee4636a19a92
Abishek Chozhan <abishekchozhan@berkeley.edu>
2021-03-07 17:36:57 -0800

    fixed location tests and added location model, but still failing tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
   it "is valid with valid attributes" do
-  	new_site = Site.new(name: "Example Location", description: "Lorem Ipsum Dolor Dot")
+  	new_site = Location.new(name: "Example Location", description: "Lorem Ipsum Dolor Dot")
   	expect(new_site).to be_valid
   end
 
   it "is not valid without a name" do
-  	new_site = Site.new(description: "Lorem Ipsum Dolor Dot")
+  	new_site = Location.new(description: "Lorem Ipsum Dolor Dot")
   	expect(new_site).to_not be_valid
   end
 
   it "is not valid without a description" do
-  	new_site = Site.new(name: "Example Location")
+  	new_site = Location.new(name: "Example Location")
   	expect(new_site).to_not be_valid
   end
 end
