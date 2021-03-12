39e71bba0bf012b82d1f652ab8790f7adecb1cea
Abishek Chozhan <abishekchozhan@berkeley.edu>
2021-03-12 00:53:28 -0800

    added five locations for testing purposes - shortened desc, proper lat&long

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   def index
-  	@locations_full = Location.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,11 @@
 
 #facilities = Facility.create([{name: "Bathroom", icon: "https://cdn1.iconfinder.com/data/icons/construction-2-13/48/87-512.png"}])
 locations = Location.create([{name: "Heron's Head Park", 
-							description: "Once slated to be a new shipping terminal in the 1970s, this slice of land ultimately became one of San
+								description: "Once slated to be a new shipping terminal in the 1970s, this slice of land ultimately became one of San
 Francisco’s natural gems thanks to a wetland restoration project in the 1990s. Heron’s Head Park is
 representative of what much of the San Francisco coastline once was: grassland uplands gently sloping
 into the pickleweed marsh bordering the bay. The pleasantly musical song of the many White-crowned
-Sparrows carries over the uplands, which are dotted with orange poppies in spring. On the mudflats a
-variety of ducks and shorebirds feed close to the trail during migration. Listen for the shrill calls from a
-pair of Black Oystercatchers that sometimes nest on the dilapidated pier in spring and summer.", 
+Sparrows carries over the uplands, which are dotted with orange poppies in spring.", 
 							longitude: "-122.373510", latitude: "37.738327"},
 							{name: "Coyote Point Recreation Area",
 								description: "This former site of an Ohlone fishing village now has a marina, a museum, and a eucalyptus forest. A
@@ -24,8 +22,20 @@ variety of sea ducks can be seen around Coyote Point, some of which are harder t
 bay. Surf Scoters, scaup (especially Greater), and Common Goldeneyes are all approachable as they dive
 for mollusks just offshore. There is sometimes a Harlequin Duck roosting on the rocks below the bluffs.
 Black Turnstones and Surfbirds scour the rocky shoreline in fall and winter, as do a few Wandering
-Tattlers from late summer through fall. The eucalyptus forest is busy with warblers and hummingbirds
-when the trees are flowering in winter, and also during migration when landbirds drop in. An
-unexpected denizen of this exotic forest is the Nuttall’s Woodpecker, which is resident. The recreation
-area has attracted many rare birds over the years including Dusky Warbler, Yellow-throated Warbler,
-and Yellow-crowned Night-Heron.", longitude:"0", latitude:"0"}])
+Tattlers from late summer through fall. ", longitude:"-122.318821", latitude:"37.587750"},
+							{name: "Martin Luther King Jr. Regional Shoreline",
+								description: "This park comprises much of the shoreline around San Leandro Bay, which is a small, sheltered area
+tucked behind Alameda and the Oakland Airport. Arrowhead Marsh is the prominent natural feature
+here. A boardwalk extending into the marsh provides one of the best opportunities to see the secretive
+Ridgway’s Rail as they emerge from the cordgrass to forage in the winding slough channels at low tide.", latitude:"37.731010", longitude:"-122.208106"},
+							{name: "Presidio",
+								description: "This area at the mouth of the San Francisco Bay offers grand views and a variety of birding. Battery
+Godfrey and Lands End west of the Golden Gate Bridge are hotspots where birds can be seen migrating
+north in spring and south in fall. Migration in spring includes swifts, warblers, orioles, and more. Many of
+the same species also pass through again in the fall, but at this time of year look especially for raptors
+crossing the Golden Gate strait from the Marin headlands. Large flocks of Vaux’s Swifts are often seen
+overhead in fall while Elegant Terns are pursued intently by Parasitic Jaegers offshore.", latitude: "37.800337", longitude: "-122.468419"},
+							{name: "Lake Merritt", description: "This lake in Oakland was originally a salt marsh named “Laguna Peralta,” and then became the first
+National Wildlife Refuge in North America in 1869. The lake is a popular wintering site for a great variety
+of waterfowl. A few Barrow’s Goldeneyes spend the winter here and the larger diving ducks such as
+Canvasback, Lesser Scaup, and Greater Scaup are unusually approachable at the shoreline.", latitude:"37.803523", longitude:"-122.257413"}])
