f6d40064598a2ae49e177cf3fd9b19335b1b64f7
Abishek Chozhan <abishekchozhan@berkeley.edu>
2021-03-12 00:40:25 -0800

    refactored index file, displaying name of location on left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
+  	@locations_full = Location.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,23 @@
 #Location Seed Data
 
 #facilities = Facility.create([{name: "Bathroom", icon: "https://cdn1.iconfinder.com/data/icons/construction-2-13/48/87-512.png"}])
-locations = Location.create([{name: "Heron's Head Pakr", description: "Once slated to be a new shipping terminal in the 1970s, this slice of land ultimately became one of San
+locations = Location.create([{name: "Heron's Head Park", 
+							description: "Once slated to be a new shipping terminal in the 1970s, this slice of land ultimately became one of San
 Francisco’s natural gems thanks to a wetland restoration project in the 1990s. Heron’s Head Park is
 representative of what much of the San Francisco coastline once was: grassland uplands gently sloping
 into the pickleweed marsh bordering the bay. The pleasantly musical song of the many White-crowned
 Sparrows carries over the uplands, which are dotted with orange poppies in spring. On the mudflats a
 variety of ducks and shorebirds feed close to the trail during migration. Listen for the shrill calls from a
 pair of Black Oystercatchers that sometimes nest on the dilapidated pier in spring and summer.", 
-longitude: "-122.373510", latitude: "37.738327"}])
+							longitude: "-122.373510", latitude: "37.738327"},
+							{name: "Coyote Point Recreation Area",
+								description: "This former site of an Ohlone fishing village now has a marina, a museum, and a eucalyptus forest. A
+variety of sea ducks can be seen around Coyote Point, some of which are harder to find elsewhere in the
+bay. Surf Scoters, scaup (especially Greater), and Common Goldeneyes are all approachable as they dive
+for mollusks just offshore. There is sometimes a Harlequin Duck roosting on the rocks below the bluffs.
+Black Turnstones and Surfbirds scour the rocky shoreline in fall and winter, as do a few Wandering
+Tattlers from late summer through fall. The eucalyptus forest is busy with warblers and hummingbirds
+when the trees are flowering in winter, and also during migration when landbirds drop in. An
+unexpected denizen of this exotic forest is the Nuttall’s Woodpecker, which is resident. The recreation
+area has attracted many rare birds over the years including Dusky Warbler, Yellow-throated Warbler,
+and Yellow-crowned Night-Heron.", longitude:"0", latitude:"0"}])
