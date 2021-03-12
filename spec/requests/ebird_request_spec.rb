1ba43deafb431427d14fb5978c29687c3b677850
Rowen <rhunter@berkeley.edy>
2021-03-11 23:51:25 -0800

    demo spaghetti

--- spec/requests/ebird_request_spec.rb
+++ spec/requests/ebird_request_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "Ebirds", type: :request do
     VCR.use_cassette('ebird/get_image_from_name') do
       VCR.use_cassette('ebird/get_bird_from_cords') do
 
-        post "/ebird", :params => { :lat => 37.42, :lng => -121.91, :rare => false, :num_req => 1}
+        post "/ebird", :params => { :lat => 37.42, :lng => -121.91, :rare => false, :num_req => 1, :num_ret => 1}
         expect(response.content_type).to eq("application/json; charset=utf-8")
-        expect(response.parsed_body()["imgsrc"]).to eq("https://upload.wikimedia.org/wikipedia/commons/4/40/Canada_goose_on_Seedskadee_NWR_%2827826185489%29.jpg")
+        expect(response.parsed_body()["imgsrc"].first).to eq("https://upload.wikimedia.org/wikipedia/commons/4/40/Canada_goose_on_Seedskadee_NWR_%2827826185489%29.jpg")
 
       end
     end
