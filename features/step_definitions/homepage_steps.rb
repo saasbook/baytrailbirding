7d4e6e50bd72cd45e3719798340c0ea2eef59456
Kevin Moy <kevinmoy@berkeley.edu>
2021-04-15 22:13:36 -0700

    Fix feature test to correspond with this

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -7,7 +7,7 @@ Then(/^I should see the sidebar cards$/) do
 end
 
 Then(/^I should see the side navigation bar$/) do
-  expect(page).to have_css("#sidenav")
+  expect(page).to have_css("div#nearby-locations-sidenav")
 end
 
 Then(/^I should see the map placeholder$/) do
