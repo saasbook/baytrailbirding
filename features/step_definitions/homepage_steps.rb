2b397082ed12764f14de0b3bf400efc36e2099c0
Kevin Moy <kevinmoy@berkeley.edu>
2021-04-28 17:29:26 -0700

    fixed CI test for map

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -11,5 +11,5 @@ Then(/^I should see the side navigation bar$/) do
 end
 
 Then(/^I should see the map placeholder$/) do
-  expect(page).to have_content("map")
+  expect(page).to have_content("Nearby Locations")
 end
