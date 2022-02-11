de88ca7d479886c25f435e9e563fbf406b1af095
Justin Cheng <justincheng101@berkeley.edu>
2022-02-10 20:08:39 -0800

    test4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include EbirdHelper
-  
   def index
     calc_location()
     @locations_full = Location.all.sort_by{|location| hav_distance([location.latitude.to_f, location.longitude.to_f], @current_location)}
